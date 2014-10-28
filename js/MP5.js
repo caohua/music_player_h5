177ca6f7bfec69646ec1b1b29d939e9f0c212cc5
hua.cao <hua.cao@vipshop.com>
2014-10-28 16:05:06 +0800

    html5 music player

---
+++ js/MP5.js
@@ -0,0 +1,227 @@
+﻿/*!
+ * version - v1.0.0 (2014-010-27)
+ * http://jraiser.org/ | Released under MIT license
+ *
+ * by caohua
+ */
+function MP5(setting){
+
+	 //配置
+	var setting = $.extend({
+		//音乐信息模版		 
+		musicInfoTpl: '',
+		autoPlay: true,//是否加载完后自动播放
+		musicList: $('#musicBox'),//音乐列表
+		musicBox:$('#mini-player'),//整个播放器
+		player:$('#musicPlayer')[0],//音乐播放器
+		musicInfoBox: $('.playing-info'),//
+		barProgressBox: $('#barProgress'),
+		totalTimeBox: $('#totalTime'),
+		curMark:'playing',//当前播放标志
+		volume: 0.7,//声音0~1
+		playModel:'order'//播放模式：single , order , circulation , and random 
+
+	},setting);
+	
+	var _player = setting.player;
+	var _musicIndex = 0;
+	var _totalTime = 0;
+	var _progressLine = 0;
+	var _playTime = 0;
+	
+	return {
+		musics: [],//保存music对象
+		autoPlay: setting.autoPlay,
+		musicTpl: setting.musicTpl,
+		musicInfoTpl: setting.musicInfoTpl,
+		musicBox: setting.musicBox,
+		musicInfoBox: setting.musicInfoBox,
+		curMark: setting.curMark,
+		barProgress: setting.barProgressBox,
+		totalTimeBox: setting.totalTimeBox,
+		playModel: setting.playModel,
+		player: setting.player,
+		volume: setting.volume,
+		//初始化
+		init: function(callback){
+			callback && callback();
+		},
+		//当准备或播放音乐时，显示具体的播放信息
+		showInfoUI: function(m){
+			var tpl = tools.createTpl(this.musicInfoTpl,m);
+			$('#cover_img').attr('src',m.picture);
+			this.musicInfoBox.html(tpl)
+		},
+		//添加音乐
+		addMusic: function(m){
+			this.musics.push(m);
+		},
+		//预先加载音乐
+		load: function(m,callback){
+			if (m){
+				_player.src = m.musicUrl;
+			}
+			callback && callback();
+		},
+		//通过索引簿预先加载音乐
+		loadByIndex: function(index,callback){
+			if (index<0){
+				index = 0;
+			}
+		
+			var musicData = this.musics[index];
+			_player.src = musicData.url;
+			this.showInfoUI(musicData);
+					//判断是否自动播放音乐
+			if (this.autoPlay){
+				this.play();
+			}
+		},
+		//播放音乐，参数(m:音乐对象，callback:回调函数)
+		play: function(m,callback){
+			var that = this;
+			_player.play();
+			$(_player).bind('playing',function(){
+				console.log('playing');
+				_totalTime = _player.duration;
+				console.log(_totalTime);
+				
+			}).bind('ended',function(){
+				//播放模式：single , order , circulation , and random 
+				switch(that.playModel){
+					case 'order': that.playNext(); break;
+					case 'single': that.paly(); break;
+					//case 'circulation': that.playNext(); break;
+				}
+			});
+			clearInterval(_progressLine);
+			this.showProgress();
+			callback && callback();
+			
+		},
+		//暂停音乐
+		pause: function(){
+			_player.pause();
+			clearInterval(_progressLine);
+			console.log('pause');
+		},
+		//通过索引来找到音乐并播放，主要是关连到具体的dom的结构
+		playByIndex: function(index,callback){
+			var maxIndex = this.musics.length-1;
+			if (index < 0 ){
+				index = maxIndex; 
+			}else if (index > maxIndex){
+				index = 0;
+			}
+			_musicIndex = index;
+			var musicData = this.musics[index];
+			_player.src = musicData.url;
+			this.showInfoUI(musicData);
+			//判断是否自动播放音乐
+			if (this.autoPlay){
+				this.play();
+			}
+			//callback && callback(music);
+			
+		},
+		//播放上一首音乐
+		playPrev: function(){
+			var that =  this;
+			var prevIndex = _musicIndex-1;
+			this.playByIndex(prevIndex);
+		},
+		//播放下一首音乐  	
+		playNext: function(){
+			var that =  this;
+			var nextIndex = _musicIndex+1;
+			this.playByIndex(nextIndex);
+		},
+		//判断播放是否暂停了
+		isPause: function(){
+			return _player.paused;
+		},
+		reload: function(){
+			_player.reload();
+		},
+		//设置音量
+		setVolume: function(vol){
+			if (typeof vol == 'number'){
+				_player.volume = vol;
+				this.musicBox.find('#btnVol').css('width',vol*100+'%');
+			}else{
+				_player.volume = this.volume;
+				this.musicBox.find('#btnVol').css('width',this.volume*100+'%');
+			}
+		},
+		getVolume: function(){
+			return _player.volume;
+		},
+		//设置静音
+		setMute: function(bool){
+			if (typeof bool == 'boolean'){
+				_player.muted = bool;
+			}else {
+				alert('数据类型不正确');
+			}
+		},
+		//获取当前播放时间
+		getCurTime: function(){
+			return _player.currentTime;
+		},
+		//获取当前总的时间
+		getTotalTime: function(curTime){
+			//curTime && _player.currentTime = curTime;
+		},
+		//显示播放进度条
+		showProgress: function(){
+			var that = this;
+
+			_progressLine = setInterval(function(){
+				var curTime = _player.currentTime;
+				var relaveTime = _totalTime - curTime;
+				var tTime = relaveTime? tools.formatTime(relaveTime*1000):'00:00';
+				var progress = curTime/_totalTime*100;
+				console.log(progress);
+				$('#totalTime').text(tTime);
+				$('#progress').css({width: progress + '%'});
+				console.log('progress');
+			},1000);
+			console.log(_progressLine);
+		}		
+	}
+}
+ 
+
+var tools = {
+	//模版映射
+	createTpl:  function(str, cfg){
+		var re = /(%(.+?)%)/g;
+		
+		return str.replace(re, function() {
+			var val = cfg[arguments[2]]+'';
+			if(typeof val == 'undefined') {
+				val = '';
+			}
+			return val;
+		});
+	},
+	
+	//判断是否为数组
+	isArry: function(v){
+		return typeof v === 'object' && typeof v.slice ==='function' && !(v.propertyIsEnumerable('length'));	
+	},	
+	
+	//格式化时间
+	formatTime: function(time){
+		time = time/1000>>0;
+		var _minute = time/60>>0 ;
+		_minute = _minute<10 ? "0"+_minute:_minute;
+		var _second = time%60<10? "0"+time%60 : time%60;
+		return _minute+":"+_second;
+	}
+
+}
+
+
+
+
---
+++ js/music.js
@@ -0,0 +1,133 @@
+﻿//douban在线数据
+var musicJson = '{"r":0,"song":[{"album":"\/subject\/2083361\/","picture":"http:\/\/img3.douban.com\/mpic\/s2932921.jpg","ssid":"889a","artist":"蔡健雅","url":"http:\/\/mr3.douban.com\/201309301020\/03e804bd70d4cc726a9412e698b8f8e1\/view\/song\/small\/p357099.mp3","company":"Warner","title":"T-time","rating_avg":4.45344,"length":344,"subtype":"","public_time":"2006","sid":"357099","aid":"2083361","sha256":"1a2b98a9a0307b43843694bf048bcd6cce6d0ec92c8fad0a534e928731f521e0","kbps":"64","albumtitle":"T-Time Tanya 蔡健...","like":1},{"album":"\/subject\/5351500\/","picture":"http:\/\/img3.douban.com\/mpic\/s6947113.jpg","ssid":"af6a","artist":"Adele","url":"http:\/\/mr4.douban.com\/201309301020\/54353ca50894fd0db94f28c911be14ec\/view\/song\/small\/p1635044.mp3","company":"Beggars","title":"Someone Like You","rating_avg":4.50857,"length":287,"subtype":"","public_time":"2011","sid":"1635044","aid":"5351500","sha256":"654f1d1de9fc681f0f2ffc0fa151794b8d8b66757f9a0df2ce754bbb5f3ea30c","kbps":"64","albumtitle":"21","like":1},{"album":"\/subject\/3184419\/","picture":"http:\/\/img3.douban.com\/view\/site\/large\/public\/ef74ced11ee3f9d.jpg","ssid":"d8e8","artist":"Lenka","url":"http:\/\/mr3.douban.com\/201309301020\/798b5fd9beaa4b23d974de5ad80a5795\/view\/song\/small\/p1381349.mp3","company":"Sony","title":"The Show","rating_avg":4.24993,"length":237,"subtype":"S","public_time":"2008","sid":"1381349","aid":"3184419","sha256":"19145181319475cbc8ad07969800192dcd3d0d5c043fd6e5f52f4e7032704f08","kbps":"64","albumtitle":"Lenka","like":1},{"album":"\/subject\/1949784\/","picture":"http:\/\/img5.douban.com\/mpic\/s1959079.jpg","ssid":"5289","artist":"蒋雅文 \/ 李逸朗","url":"http:\/\/mr5.douban.com\/201309301020\/78567d522ef159b0368583b187127b40\/view\/song\/small\/p349651.mp3","company":"英皇娱乐","title":"爱的故事上集","rating_avg":3.85113,"length":234,"subtype":"","public_time":"2006","sid":"349651","aid":"1949784","sha256":"eb6aad741ea607b5be3d3e488909a89db9fbb91deb6549e387b81ae0af56990d","kbps":"64","albumtitle":"冬之恋人","like":1},{"album":"\/subject\/3603814\/","picture":"http:\/\/img3.douban.com\/mpic\/s3666735.jpg","ssid":"99c1","artist":"陈奕迅","url":"http:\/\/mr3.douban.com\/201309301020\/d2f18c2484eb3729d9dfd9bdd21eefb6\/view\/song\/small\/p1383788.mp3","company":"Universal","title":"不来也不去","rating_avg":4.23671,"length":269,"subtype":"","public_time":"2009","sid":"1383788","aid":"3603814","sha256":"46d4afe69619f815e1be9d2cc8cbba766e1f9af050c43db59b967a0202331cc2","kbps":"64","albumtitle":"H³M","like":1},{"album":"\/subject\/6904929\/","picture":"http:\/\/img3.douban.com\/mpic\/s6998672.jpg","ssid":"7540","artist":"蔡健雅","url":"http:\/\/mr3.douban.com\/201309301020\/33d5b7c6e50acc032f0b310f35acedd2\/view\/song\/small\/p1746117.mp3","company":"亚神","title":"说到爱","rating_avg":4.03438,"length":266,"subtype":"","public_time":"2011","sid":"1746117","aid":"6904929","sha256":"ed7c4b136f37f63ff8407edb7d72aff5233c0ba3a2b68691d072968efb6c21bf","kbps":"64","albumtitle":"說到愛","like":1},{"picture":"http:\/\/img3.douban.com\/view\/dale-online\/dale_ad\/public\/167644b41e4166f.jpg","albumtitle":"豆瓣FM","adtype":3,"monitor_url":"","album":"http:\/\/erebor.douban.com\/redirect\/?ad=58923&uid=48120885&bid=8KPDlHEd%2BvI&unit=dale_fm_audio&crtr=4%3A-3&cfg=7afa9e1cc93afb9ffe31d2df4fdf7577ad475e11&ns=1380507609223839000&target=http%3A%2F%2Fdouban.fm%2Fapp%3Fref%3Dsoundad","like":"0","title":"豆瓣FM APP IPHONE3.1版","url":"http:\/\/mr4.douban.com\/201309301020\/f6a9f5c2b17244de62ac2b019c21fcb4\/rda\/9c7cef4316a828d.mp3","artist":"豆瓣FM","subtype":"T","length":17,"sid":"da58923_29","aid":"89058923"},{"album":"\/subject\/2995812\/","picture":"http:\/\/img3.douban.com\/mpic\/s2967252.jpg","ssid":"0148","artist":"Jason Mraz","url":"http:\/\/mr3.douban.com\/201309301020\/626cc6e6f3876066c5d8e4c0f743e817\/view\/song\/small\/p107686.mp3","company":"Warner","title":"Im Yours","rating_avg":4.53269,"length":242,"subtype":"","public_time":"2008","sid":"107686","aid":"2995812","sha256":"b3599b8ffc577d53207baf96ed9eb6db2bf1bd7444e0fc6622f87b3309ff7055","kbps":"64","albumtitle":"We Sing. We Dance...","like":1},{"album":"\/subject\/3292308\/","picture":"http:\/\/img3.douban.com\/mpic\/s3356170.jpg","ssid":"8873","artist":"卫兰","url":"http:\/\/mr4.douban.com\/201309301020\/fcdad57204ad01913a5b5b9d66644993\/view\/song\/small\/p1382624.mp3","company":"东亚唱片","title":"就算世界无童话","rating_avg":4.02391,"length":262,"subtype":"","public_time":"2008","sid":"1382624","aid":"3292308","sha256":"44b5a450668f1465ff2b7a8c5b635b63c6db009263d9247b182bedb60b98953f","kbps":"64","albumtitle":"Serving You","like":1},{"album":"\/subject\/3259584\/","picture":"http:\/\/img3.douban.com\/mpic\/s3332673.jpg","ssid":"ffc2","artist":"Beyoncé","url":"http:\/\/mr3.douban.com\/201309301020\/de27385d057b78d36320d11bd6f92f18\/view\/song\/small\/p1380740.mp3","company":"Sony","title":"If I Were A Boy","rating_avg":4.19261,"length":250,"subtype":"","public_time":"2008","sid":"1380740","aid":"3259584","sha256":"7addc4b39b47e85e0f58a82027d5b132cf36df1fc29f1399b62bec2284815cbc","kbps":"64","albumtitle":"I Am… Sasha Fierce","like":1},{"album":"\/subject\/3815932\/","picture":"http:\/\/img3.douban.com\/mpic\/s3850703.jpg","ssid":"54dd","artist":"王菲","url":"http:\/\/mr3.douban.com\/201309301020\/4fc5aa7a599e33035c1003f228fbc1a2\/view\/song\/small\/p1395259.mp3","company":"Universal","title":"暖昧","rating_avg":4.68337,"length":281,"subtype":"","public_time":"2009","sid":"1395259","aid":"3815932","sha256":"1d35356213918a519d24381f8ad674ae70581c857d2e32fdf91f12a34056e0c4","kbps":"64","albumtitle":"阿菲正传","like":1},{"album":"\/subject\/1494852\/","picture":"http:\/\/img3.douban.com\/mpic\/s3267022.jpg","ssid":"337d","artist":"BLUE","url":"http:\/\/mr3.douban.com\/201309301020\/6ab14ac067f1034e5d31618ace7ec40b\/view\/song\/small\/p663918.mp3","company":"EMI","title":"U Make Me Wanna","rating_avg":4.39641,"length":220,"subtype":"","public_time":"2004","sid":"663918","aid":"1494852","sha256":"ca0cff26788720f78c8514ea861e5410f179ff6ca2233f8ff68bc3fb8819b4ab","kbps":"64","albumtitle":"Best of Blue","like":1},{"album":"\/subject\/3041487\/","picture":"http:\/\/img3.douban.com\/mpic\/s2874587.jpg","ssid":"dcd7","artist":"王若琳","url":"http:\/\/mr3.douban.com\/201309301020\/5942c2ca620a475cb6adb6b342ca2035\/view\/song\/small\/p187766.mp3","company":"Sony","title":"有你的快乐","rating_avg":4.35782,"length":197,"subtype":"","public_time":"2008","sid":"187766","aid":"3041487","sha256":"ca457e021c86fc1d546a4def27eb97a5817fb4b256dd2399e3509e27f98f4c6e","kbps":"64","albumtitle":"Start from Here","like":1},{"album":"\/subject\/10808199\/","picture":"http:\/\/img3.douban.com\/mpic\/s10387532.jpg","ssid":"ef2b","artist":"薛凯琪","url":"http:\/\/mr3.douban.com\/201309301020\/bc5ddbfb06fb77753f0a2b8e939c3717\/view\/song\/small\/p1880667.mp3","company":"Warner Music Hong Kong","title":"Better Me (国语)","rating_avg":4.43123,"length":226,"subtype":"","public_time":"2012","sid":"1880667","aid":"10808199","sha256":"6f32b4b50a0e013f593ddc1558f41664b15012092533a0ed14f25e5203152296","kbps":"64","albumtitle":"Better me","like":1},{"album":"\/subject\/11611661\/","picture":"http:\/\/img3.douban.com\/mpic\/s11373263.jpg","ssid":"cd4f","artist":"陈绮贞","url":"http:\/\/mr3.douban.com\/201309301020\/86f1f84197157fd1e50f0da044c98e80\/view\/song\/small\/p1882505.mp3","company":"Avex Taiwan","title":"旅行的意义","rating_avg":4.50249,"length":262,"subtype":"","public_time":"2012","sid":"1882505","aid":"11611661","sha256":"e02cacbe5ffa1997ca5ed0da620b4371811f07e64234dc8a8b89c0d7ecfd2513","kbps":"64","albumtitle":"夏季練習曲世界巡...","like":1},{"album":"\/subject\/4829750\/","picture":"http:\/\/img3.douban.com\/mpic\/s4370242.jpg","ssid":"3aae","artist":"Jewel","url":"http:\/\/mr3.douban.com\/201309301020\/dec4f6954212ff27c8487a8641266fbc\/view\/song\/small\/p1478796.mp3","company":"The Valory Music Co.","title":"Stay Here Forever","rating_avg":3.86562,"length":179,"subtype":"","public_time":"2010","sid":"1478796","aid":"4829750","sha256":"267761a3c8ce21baafdc90c0a53017ee0edc76f0cef17cb6d8445a02397b4c55","kbps":"64","albumtitle":"Sweet and Wild","like":1},{"album":"\/subject\/3259584\/","picture":"http:\/\/img3.douban.com\/mpic\/s3332673.jpg","ssid":"a476","artist":"Beyoncé","url":"http:\/\/mr5.douban.com\/201309301020\/d31be9b92e52ac9fe1e616330e35a1f6\/view\/song\/small\/p1380741.mp3","company":"Sony","title":"Halo","rating_avg":4.19261,"length":261,"subtype":"","public_time":"2008","sid":"1380741","aid":"3259584","sha256":"68a53f5a2f9d099ae3ac2f426a835d40d64a41ee96bd3d0945a5070a2ea8f24c","kbps":"64","albumtitle":"I Am… Sasha Fierce","like":1},{"album":"\/subject\/1402698\/","picture":"http:\/\/img3.douban.com\/mpic\/s3232226.jpg","ssid":"d656","artist":"蔡依林","url":"http:\/\/mr4.douban.com\/201309301020\/fb9e5675ba4bfbea9c26fa2e5403600b\/view\/song\/small\/p617692.mp3","company":"Sony","title":"天空","rating_avg":3.38821,"length":278,"subtype":"","public_time":"2005","sid":"617692","aid":"1402698","sha256":"c63c3d467fb09bee97a4e7e3a9a2bcbaddfe1a3fcded6ba569071d63017206be","kbps":"64","albumtitle":"野蛮游戏","like":1},{"album":"\/subject\/1438805\/","picture":"http:\/\/img3.douban.com\/mpic\/s27066027.jpg","ssid":"8409","artist":"卫兰","url":"http:\/\/mr4.douban.com\/201309301020\/738356e4838ce5c5ba33d78b7cdbf9f2\/view\/song\/small\/p696512.mp3","company":"A Music","title":"情深说话未曾讲","rating_avg":4.20193,"length":255,"subtype":"","public_time":"2005","sid":"696512","aid":"1438805","sha256":"4d626aeceaffcaff31543314deb440b2e109414f3623ecf772d9475622bac41e","kbps":"64","albumtitle":"Day & Night","like":1},{"album":"\/subject\/1428725\/","picture":"http:\/\/img3.douban.com\/mpic\/s2413675.jpg","ssid":"2859","artist":"陈奕迅","url":"http:\/\/mr3.douban.com\/201309301020\/ca48bb1bc27d1cacadf5a2cb1c13e0aa\/view\/song\/small\/p15168.mp3","company":"英皇娱乐","title":"单车","rating_avg":4.58094,"length":210,"subtype":"","public_time":"2001","sid":"15168","aid":"1428725","sha256":"a6c5e669c4b5a270088ccbfb0f57c715f7556d40f9e5bd813ee43ef14eaae5a5","kbps":"64","albumtitle":"Shall We Dance? S...","like":1},{"album":"\/subject\/5952615\/","picture":"http:\/\/img3.douban.com\/mpic\/s4616653.jpg","ssid":"bffd","artist":"Cee-Lo","url":"http:\/\/mr4.douban.com\/201309301020\/cfec45e1132d9fec18a691da6d8fd06f\/view\/song\/small\/p1562724.mp3","company":"Elektra Entertainment","title":"Forget You","rating_avg":4.04155,"length":222,"subtype":"","public_time":"2011","sid":"1562724","aid":"5952615","sha256":"2f9a7b0fc949ed2723965308aa4bb3704cb9bad332012209fa6dfe35e36b5a20","kbps":"64","albumtitle":"2011 Grammy Nominees","like":1},{"album":"\/subject\/1867567\/","picture":"http:\/\/img3.douban.com\/mpic\/s3659503.jpg","ssid":"f944","artist":"Dido","url":"http:\/\/mr3.douban.com\/201309301020\/cf9f150577ab7ea74d371c1c9f1f9853\/view\/song\/small\/p1029117.mp3","company":"EMI","title":"Thank You","rating_avg":4.01412,"length":226,"subtype":"","public_time":"2000","sid":"1029117","aid":"1867567","sha256":"c53c8b75f4b30765bdcfb6eef34e641f83d128ae1255bc19c6af6ef0bec74b86","kbps":"64","albumtitle":"One Step Too Far","like":1},{"album":"\/subject\/4813565\/","picture":"http:\/\/img3.douban.com\/mpic\/s4343572.jpg","ssid":"d921","artist":"严爵","url":"http:\/\/mr4.douban.com\/201309301020\/5a471e48709e037b0e1925f23fe41001\/view\/song\/small\/p1468962.mp3","company":"相信音乐","title":"谢谢你的美好","rating_avg":3.43037,"length":193,"subtype":"","public_time":"2010","sid":"1468962","aid":"4813565","sha256":"1b7e4553b111077feb23e2c2bab7b28b32b3012f4199e91f678522536bf9ad57","kbps":"64","albumtitle":"謝謝你的美好","like":1},{"album":"\/subject\/3870258\/","picture":"http:\/\/img3.douban.com\/mpic\/s3912096.jpg","ssid":"ef93","artist":"苏打绿","url":"http:\/\/mr3.douban.com\/201309301020\/59d8f948a70320641c9f4231e4d50aac\/view\/song\/small\/p1467236.mp3","company":"林暐哲音乐社","title":"频率","rating_avg":4.76544,"length":349,"subtype":"","public_time":"2006","sid":"1467236","aid":"3870258","sha256":"392c6d96d014a524925d202d4deadd7f1f85b85fc484f28948f9315275718cc3","kbps":"64","albumtitle":"频率lalala","like":1},{"album":"\/subject\/1421922\/","picture":"http:\/\/img4.douban.com\/mpic\/s1661548.jpg","ssid":"0375","artist":"Daniel Powter","url":"http:\/\/mr3.douban.com\/201309301020\/44c66bab4d7bf544c1c5b495eec9abff\/view\/song\/small\/p280187.mp3","company":"Warner","title":"Free Loop","rating_avg":4.31581,"length":228,"subtype":"","public_time":"2006","sid":"280187","aid":"1421922","sha256":"525fae725ff55c1c4b705a5b8ee3b97309c6865164b77f806350fc438e101dcf","kbps":"64","albumtitle":"Daniel Powter","like":1},{"album":"\/subject\/2300122\/","picture":"http:\/\/img3.douban.com\/mpic\/s4716765.jpg","ssid":"ae1f","artist":"Michael Jackson","url":"http:\/\/mr4.douban.com\/201309301020\/e9e74d14dd71a10462011042de0bfda7\/view\/song\/small\/p431324.mp3","company":"Sony","title":"You Are Not Alone","rating_avg":4.76038,"length":276,"subtype":"","public_time":"2003","sid":"431324","aid":"2300122","sha256":"3d12ac2309ce4a7b9317762aa6fb47dc8472566ca0037d7be73814921a587bb0","kbps":"64","albumtitle":"Number Ones","like":1},{"album":"\/subject\/5952615\/","picture":"http:\/\/img3.douban.com\/mpic\/s4616653.jpg","ssid":"e1b2","artist":"Bruno Mars \/ B.o.B","url":"http:\/\/mr3.douban.com\/201309301020\/315adc065744ea7947ecf758a3a19039\/view\/song\/small\/p1562725.mp3","company":"EMI","title":"Nothin On You","rating_avg":4.04155,"length":267,"subtype":"","public_time":"2011","sid":"1562725","aid":"5952615","sha256":"2422b6fa22611a7858060fd9c238e679626b3173bb0d161258b4175d69f17473","kbps":"64","albumtitle":"2011 Grammy Nominees","like":1},{"album":"\/subject\/1850252\/","picture":"http:\/\/img3.douban.com\/mpic\/s1741604.jpg","ssid":"1f6f","artist":"泳儿","url":"http:\/\/mr3.douban.com\/201309301020\/6da45a51f63d973cfc828fa8393f7971\/view\/song\/small\/p747549.mp3","company":"英皇娱乐","title":"感应","rating_avg":3.79953,"length":257,"subtype":"","public_time":"2006","sid":"747549","aid":"1850252","sha256":"1ca65fceedc5729abcf95b0b0dc3e939396e80314bf2f1f606cba614fe1a7a29","kbps":"64","albumtitle":"感应","like":1},{"album":"\/subject\/5258181\/","picture":"http:\/\/img3.douban.com\/mpic\/s4464236.jpg","ssid":"ae28","artist":"Bruno Mars","url":"http:\/\/mr4.douban.com\/201309301020\/36cd75a0c8479bcc99ef6f6814493132\/view\/song\/small\/p1562734.mp3","company":"Warner","title":"Just The Way You Are","rating_avg":4.33807,"length":219,"subtype":"","public_time":"2010","sid":"1562734","aid":"5258181","sha256":"325664c176d669a0d7280741008ee76efecc4c1fd9967391e193d2bb91893cc2","kbps":"64","albumtitle":"Doo-Wops & Hooligans","like":1},{"album":"\/subject\/1958713\/","picture":"http:\/\/img3.douban.com\/mpic\/s1987011.jpg","ssid":"e20e","artist":"吴雨霏","url":"http:\/\/mr3.douban.com\/201309301020\/d2f1a83ac06b2b73d02ab74ab147c9a5\/view\/song\/small\/p345040.mp3","company":"EMI","title":"明知做戏","rating_avg":3.87253,"length":270,"subtype":"","public_time":"2006","sid":"345040","aid":"1958713","sha256":"ff5fdbfaf3e3ece12b4a1d49e087f1a7aee0877b07c800cfc3510882bef05032","kbps":"64","albumtitle":"With A Boy Like You","like":1},{"album":"\/subject\/1414192\/","picture":"http:\/\/img3.douban.com\/mpic\/s4700654.jpg","ssid":"8c8b","artist":"蔡淳佳","url":"http:\/\/mr4.douban.com\/201309301020\/6bda65e73df9ddb1f7a246598099852c\/view\/song\/small\/p664913.mp3","company":"Warner","title":"陪我看日出","rating_avg":3.99016,"length":258,"subtype":"","public_time":"2004","sid":"664913","aid":"1414192","sha256":"606c1cdbec28707d13ed1dc532c2202b7b9641aea72d7b29a576431fbb9eb190","kbps":"64","albumtitle":"日出","like":1},{"album":"\/subject\/1547236\/","picture":"http:\/\/img3.douban.com\/mpic\/s4712614.jpg","ssid":"cfb3","artist":"小肥","url":"http:\/\/mr3.douban.com\/201309301020\/84b6f141d85800434aa85a8369174391\/view\/song\/small\/p632670.mp3","company":"正东唱片","title":"无赖 (原唱:主唱:郑中基","rating_avg":4.0402,"length":214,"subtype":"","public_time":"2006","sid":"632670","aid":"1547236","sha256":"51a603a3fb70cd05ed06c600b39e3971f61598e195dd413de978229713a40bf4","kbps":"64","albumtitle":"自作自乐 Demo Pro...","like":1}]}';
+var _doubanMusic = JSON.parse(musicJson);
+
+$(function(){
+	/*加载js*/
+	function loadScript(url,callback){
+		var script = document.createElement('script');
+		script.type = 'text/plain';
+		script.id = 'douban1';
+		if (script.readyState){ //for ie
+			script.onreadystatechange = function(){
+				if (script.readyState == 'loaded' || script.readyState == 'complete'){
+					script.onreadystatechange = null;
+					var js = script.innerHTML;
+					alert(js);
+					callback && callback();
+				}
+			}
+		}else{//for firefox chrome opera
+			script.onload = function(){
+				callback && callback();
+			}
+		}
+		script.src = url;
+		document.body.appendChild(script);
+	}
+	
+	var mp5 = new MP5({
+		autoPlay: true,//是否加载完后自动播放
+		musicList: $('#musicBox'),//音乐列表
+		musicBox:$('#mini-player'),//整个播放器
+		player:$('#musicPlayer')[0],//音乐播放器
+		musicInfoBox: $('#music_data'),//
+		barProgressBox: $('#progress'),
+		totalTimeBox: $('#totalTime'),
+		curMark:'playing',//当前播放标志
+		volume: 0.2,//声音0~1
+		playModel:'order',//播放模式：single , order , circulation , and random 
+		musicInfoTpl:['<div class="author">',
+			 '<p class="artist">%artist%</p>',
+				'<p class="album">',
+					'<em class="album_title">< %albumtitle% ></em>',
+					'<time class="public_time">%public_time%</time>',
+				'</p>',
+			'</div>',
+			'<div class="player">',
+				'<p class="title">%title%</p>',
+				'<div class="progress_box">',
+					'<div class="progress" style="width:0px;" id="progress"></div>',
+				'</div>',
+				'<p class="player_info">',
+					'<em class="play_time" id="totalTime">00:00</em>',
+					'<a href="#" class="player_icon volume_icon"  aria-hidden="true" data-icon="&#xe044;"></a>',
+				'</p>',
+			'</div>'].join(''),
+
+	});
+	
+	var songs = _doubanMusic.song;
+	for (var i=0,len=songs.length; i<len; i++){
+		var song = songs[i];
+		var picture = song.picture;
+		picture = picture.replace('img3','img5');
+		picture = picture.replace('mpic','lpic');
+
+		RGBaster.colors(picture, {
+          exclude: [ 'rgb(255,255,255)', 'rgb(0,0,0)' ],
+          success: function(payload) {
+            // 容器的背景色变成图片的主色 - payload.dominant
+            //$(".player_bg").css("background", payload.dominant);
+          }
+        });    
+		song.picture = picture;
+		mp5.addMusic(song);
+	}
+	
+	//处理具体业务逻辑
+	mp5.init(function(){
+
+		mp5.setVolume();
+		
+		$('#playBtn').bind('click',function(){
+			if (mp5.isPause()){
+				mp5.play();
+				mp5.musicBox.find('#playBtn').addClass('pause');
+				
+			}else {
+				mp5.pause();
+				mp5.musicBox.find('#playBtn').removeClass('pause');
+			}
+		});
+		
+		$('#nextBtn').bind('click',function(){
+			mp5.playNext();
+			return false;
+		});
+		
+		$('#btnMuted').bind('click',function(){
+			if ($(this).hasClass('showMuted')){
+				mp5.setMute(false);
+				$(this).removeClass('showMuted');
+			}else{
+				mp5.setMute(true);
+				$(this).addClass('showMuted');
+				
+			}
+			
+			return false;
+		});
+		
+		$('#volBar').bind('click',function(event){
+			var offset = $(this).offset();
+			var allLen = 50;
+			var rLen = event.clientX - offset.left;
+			
+			var progress = rLen/allLen*100;
+			var player = mp5.player;
+			var vol = rLen/allLen;
+			mp5.setVolume(vol);
+			
+		});
+
+		$('#loveBtn').bind('click',function(event){
+			$(this).addClass('like');
+			return false;
+		});
+
+		//加载第一首歌
+		mp5.loadByIndex(0);
+		
+	});
+});
